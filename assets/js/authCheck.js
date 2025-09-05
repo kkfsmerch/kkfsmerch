8070c946dc336ec3e7769cc7d93bc22920bd1c46
KKFS <kkfsmerch@kkfs.org>
2025-09-06 00:56:16 +0900

    Update authCheck.js

--- assets/js/authCheck.js
+++ assets/js/authCheck.js
@@ -3,14 +3,14 @@ import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebase
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDPrE0pFoclbxzAMeRZkha8AEm-TWv8BQ",
-  authDomain: "kkfs-merch.firebaseapp.com",
-  projectId: "kkfs-merch",
-  storageBucket: "kkfs-merch.firebasestorage.app",
-  messagingSenderId: "39903297001",
-  appId: "1:39903297001:web:45449b15644c272c661f78",
-  measurementId: "G-8XEY41KLSP"
-};
+    apiKey: "AIzaSyCo_9U-Ged9xcsMwnRlNNoSJc6mV84KyaY",
+    authDomain: "kkfs-merch-55e11.firebaseapp.com",
+    projectId: "kkfs-merch-55e11",
+    storageBucket: "kkfs-merch-55e11.firebasestorage.app",
+    messagingSenderId: "324569689619",
+    appId: "1:324569689619:web:1e0763d6a05010610559e6",
+    measurementId: "G-63HP6N5Q9L"
+  };
 
 // Initialize Firebase (or use existing app)
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
@@ -21,4 +21,5 @@ onAuthStateChanged(auth, (user) => {
   if (!user && window.location.pathname !== '/login.html') {
     window.location.href = "login.html";
   }
+
 });
