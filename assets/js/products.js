0bd40681a39a73b5605a9c9bbf54e7ac748edb11
KKFS <kkfsmerch@kkfs.org>
2025-09-06 09:58:35 +0900

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,12 +1,17 @@
 // products.js - sample dataset
 const PRODUCTS = [
   // Athletic
-  {id:'blue navy hoodie', name:'Blue Hoodie', category:'athletic', type:'tops', price:40000, limited:true, desc:'Navy Hoodie', sizes:['S','M','L','XL'], stock:{S:0,M:0,L:0,XL:0}, img:'assets/images/bluehoodie.png', createdAt:'2025-08-02'},
+  //{id:'blue navy hoodie', name:'Blue Hoodie', category:'athletic', type:'tops', price:40000, limited:true, desc:'Navy Hoodie', sizes:['S','M','L','XL'], stock:{S:0,M:0,L:0,XL:0}, img:'assets/images/bluehoodie.png', createdAt:'2025-08-02'},
 
   // General
-  {id:'black fleece', name:'Fleece', category:'general', type:'tops', price:45000, limited:false, desc:'Black fleece', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/fleece.png', createdAt:'2025-09-02'},
+  //{id:'shirt one', name:'Shirt 1', category:'general', type:'tops', price:23000, limited:false, desc:'T shirt merch', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/shirt1.png', createdAt:'2025-09-06'},
 
   // Seasonal
-  {id:'Merlins T-Shirt', name:'Black T-Shirt', category:'seasonal', type:'tops', price:25000, limited:false, desc:'Black T-Shirt', sizes:['S','M','L','XL'], stock:{S:Infinity,M:Infinity,L:Infinity,XL:Infinity}, img:'assets/images/tshirt.png', createdAt:'2025-09-02'},
+  {id:'shirt one', name:'Shirt 1', category:'seasonal', type:'tops', price:23000, limited:false, desc:'T shirt merch', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/shirt1.png', createdAt:'2025-09-06'},
+  {id:'shirt two', name:'Shirt 2', category:'seasonal', type:'tops', price:23000, limited:false, desc:'T shirt merch', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/shirt2.png', createdAt:'2025-09-06'},
+  {id:'standard', name:'Standard', category:'seasonal', type:'tops', price:23000, limited:false, desc:'T shirt merch', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/standard.png', createdAt:'2025-09-06'},
+  {id:'hoodie', name:'Hoodie', category:'seasonal', type:'tops', price:42000, limited:false, desc:'Merlins Hoodie', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/hoodie.png', createdAt:'2025-09-06'},
+
+
 ];
 window.PRODUCTS = PRODUCTS;
