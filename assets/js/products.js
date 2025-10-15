b93d6bb493e197da9bbf6c8827850565b2b88ce7
KKFS <kkfsmerch@kkfs.org>
2025-10-15 18:26:31 +0900

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -6,11 +6,11 @@ const PRODUCTS = [
   // General
   //{id:'shirt one', name:'Shirt 1', category:'general', type:'tops', price:23000, limited:false, desc:'T shirt merch', sizes:['S','M','L'], stock:{S:Infinity,M:Infinity,L:Infinity}, img:'assets/images/shirt1.png', createdAt:'2025-09-06'},
 
-  {id:'whitekeyring', name:'KKFS Merlins key rings', category:'general', type:'etc', price:5000, limited:false, desc:'White KKFS Merlins key rings', sizes:['Free'], stock:{Free:9}, img: 'images/accessories/keychain/whitekeyring.png', createdAt:'2025-09-06'},
+  {id:'whitekeyring', name:'KKFS Merlins key rings', category:'general', type:'etc', price:5000, limited:false, desc:'White KKFS Merlins key rings', sizes:['Free'], stock:{Free:9}, img: 'assets/images/accessories/keychain/whitekeyring.png', createdAt:'2025-09-06'},
   {id:'blackkeyring', name:'KKFS Merlins key rings', category:'general', type:'etc', price:5000, limited:false, desc:'Black KKFS Merlins key rings', sizes:['Free'], stock:{Free:2}, img:'assets/images/accessories/keychain/blackkeyring.png', createdAt:'2025-09-06'},
-  {id:'lanyard', name:'KKFS Lanyards', category:'general', type:'etc', price:5000, limited:false, desc:'KKFS Lanyards', sizes:['Free'], stock:{Free:Infinity}, img:'assets/images/accessories/keychain/lanyard.png', createdAt:'2025-09-06'},
-  {id:'hat', name:'Embroidered Merlins Hat', category:'general', type:'etc', price:30000, limited:false, desc:'White embroidered hat', sizes:['Adult'], stock:{Adult:Infinity}, img:'assets/images/accessories/keychain/hat.png', createdAt:'2025-09-06'},
-  {id:'regularhat', name:'Printed Merlins Hat', category:'general', type:'etc', price:23000, limited:false, desc:'White Printed Merlins Hat', sizes:['Kids', 'Adult'], stock:{Kids: Infinity, Adult:Infinity}, img:'assets/images/accessories/keychain/regularhat.png', createdAt:'2025-09-06'},
+  {id:'lanyard', name:'KKFS Lanyards', category:'general', type:'etc', price:5000, limited:false, desc:'KKFS Lanyards', sizes:['Free'], stock:{Free:Infinity}, img:'assets/images/accessories/lanyard/lanyard.png', createdAt:'2025-09-06'},
+  {id:'hat', name:'Embroidered Merlins Hat', category:'general', type:'etc', price:30000, limited:false, desc:'White embroidered hat', sizes:['Adult'], stock:{Adult:Infinity}, img:'assets/images/accessories/hat/hat.png', createdAt:'2025-09-06'},
+  {id:'regularhat', name:'Printed Merlins Hat', category:'general', type:'etc', price:23000, limited:false, desc:'White Printed Merlins Hat', sizes:['Kids', 'Adult'], stock:{Kids: Infinity, Adult:Infinity}, img:'assets/images/accessories/hat/regularhat.png', createdAt:'2025-09-06'},
   // Seasonal
   {id:'shirt one', name:'Shirt 1', category:'seasonal', type:'tops', price:32000, limited:false, desc:'T shirt merch', sizes:['S','M','L','XL'], stock:{S:Infinity,M:Infinity,L:Infinity, XL:Infinity}, img:'assets/images/shirt/shirt1/shirt1og.png', createdAt:'2025-09-06'},
   {id:'shirt two', name:'Shirt 2', category:'seasonal', type:'tops', price:32000, limited:false, desc:'T shirt merch', sizes:['S','M','L','XL'], stock:{S:Infinity,M:Infinity,L:Infinity, XL:Infinity}, img:'assets/images/shirt/shirt2/shirt2.png', createdAt:'2025-09-06'},
